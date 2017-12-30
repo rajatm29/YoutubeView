7a4aa6023598353486af99b2f7befd3701f8281e
Rajat Monga <rajatmonga29@gmail.com>
2017-12-30 17:34:37 -0600

    Update MainActivity.kt

--- YoutubeKotlin/app/src/main/java/com/example/rajatmonga/youtubekotlin/MainActivity.kt
+++ YoutubeKotlin/app/src/main/java/com/example/rajatmonga/youtubekotlin/MainActivity.kt
@@ -16,13 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //recyclerView_main.setBackgroundColor(Color.BLUE)
-
         recyclerView_main.layoutManager = LinearLayoutManager(this)
         //recyclerView_main.adapter = MainAdapter()
-
-
         fetchJson()
     }
 
