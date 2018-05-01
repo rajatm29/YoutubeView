c71468e8f9c037b6a999d3f795eb14f932261ba6
Rajat Monga <rajatmonga29@gmail.com>
2018-04-30 20:08:07 -0500

    Update Models.kt

--- YoutubeKotlin/app/src/main/java/com/example/rajatmonga/youtubekotlin/Models.kt
+++ YoutubeKotlin/app/src/main/java/com/example/rajatmonga/youtubekotlin/Models.kt
@@ -3,6 +3,7 @@ package com.example.rajatmonga.youtubekotlin
 /**
  * Created by rajatmonga on 1/4/18.
  */
+
 class HomeFeed(val videos: List<Video>)
 
 class Video(val id: Int, val name:String, val link: String, val imageUrl: String,
